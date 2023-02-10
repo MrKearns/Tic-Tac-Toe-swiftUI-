36da973826a44b579e8bc7262230e1ebc77e0970
MrKearns <jkearns631@students.mchenry.edu>
2023-02-10 09:12:40 -0600

    Initial Commit

---
+++ Tic-Tac-Toe(swiftUI)/Tic_Tac_Toe_swiftUI_App.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_Toe_swiftUI_App.swift
+//  Tic-Tac-Toe(swiftUI)
+//
+//  Created by Jonathan Kearns on 2/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_Toe_swiftUI_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
